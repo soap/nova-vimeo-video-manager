e5f53474b29bb748c0d913a4a00f7f23d79c190f
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-11-10 01:31:48 +0700

    Update tool

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -1,35 +1,38 @@
 <template>
     <div>
-        <heading class="mb-6">Vimeo Video Manager</heading>
+    <svg class="sidebar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+        <path fill="var(--sidebar-icon)"
+              d="M7 10V7a5 5 0 1 1 10 0v3h2a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h2zm2 0h6V7a3 3 0 0 0-6 0v3zm-4 2v8h14v-8H5zm7 2a1 1 0 0 1 1 1v2a1 1 0 0 1-2 0v-2a1 1 0 0 1 1-1z"/>
+    </svg>
+    <span class="sidebar-label">
+            {{ __('Vimemo Video Manager') }}
+    </span>
 
-        <card
-            class="bg-90 flex flex-col items-center justify-center"
-            style="min-height: 300px"
-        >
-            <svg
-                class="spin fill-80 mb-6"
-                width="69"
-                height="72"
-                viewBox="0 0 23 24"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <path
-                    d="M20.12 20.455A12.184 12.184 0 0 1 11.5 24a12.18 12.18 0 0 1-9.333-4.319c4.772 3.933 11.88 3.687 16.36-.738a7.571 7.571 0 0 0 0-10.8c-3.018-2.982-7.912-2.982-10.931 0a3.245 3.245 0 0 0 0 4.628 3.342 3.342 0 0 0 4.685 0 1.114 1.114 0 0 1 1.561 0 1.082 1.082 0 0 1 0 1.543 5.57 5.57 0 0 1-7.808 0 5.408 5.408 0 0 1 0-7.714c3.881-3.834 10.174-3.834 14.055 0a9.734 9.734 0 0 1 .03 13.855zM4.472 5.057a7.571 7.571 0 0 0 0 10.8c3.018 2.982 7.912 2.982 10.931 0a3.245 3.245 0 0 0 0-4.628 3.342 3.342 0 0 0-4.685 0 1.114 1.114 0 0 1-1.561 0 1.082 1.082 0 0 1 0-1.543 5.57 5.57 0 0 1 7.808 0 5.408 5.408 0 0 1 0 7.714c-3.881 3.834-10.174 3.834-14.055 0a9.734 9.734 0 0 1-.015-13.87C5.096 1.35 8.138 0 11.5 0c3.75 0 7.105 1.68 9.333 4.319C16.06.386 8.953.632 4.473 5.057z"
-                    fill-rule="evenodd"
-                />
-            </svg>
+<ul class="mb-8 list-reset">
 
-            <h1 class="text-white text-4xl text-90 font-light mb-6">
-                We're in a black hole.
-            </h1>
+        <li class="mb-4 text-sm leading-wide">
+            <router-link :to="{
+                name: 'index',
+                params: {
+                    resourceName: 'folders'
+                }
+            }" class="ml-8 text-white no-underline dim">
+                {{ __('Roles') }}
+            </router-link>
+        </li>
 
-            <p class="text-white-50% text-lg">
-                You can edit this tool's component at:
-                <code class="ml-1 border border-80 text-sm font-mono text-white bg-black rounded px-2 py-1">
-                    /nova-components/VimeoVideoManager/resources/js/components/Tool.vue
-                </code>
-            </p>
-        </card>
+        <li class="mb-4 text-sm leading-wide">
+            <router-link :to="{
+                name: 'index',
+                params: {
+                    resourceName: 'videos'
+                }
+            }" class="ml-8 text-white no-underline dim">
+                {{ __('Permissions') }}
+            </router-link>
+        </li>
+
+</ul>
     </div>
 </template>
 
