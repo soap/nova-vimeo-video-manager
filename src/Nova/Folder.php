9f5172b8f5b15a03c6f498d32da19d36196ac953
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-11-10 01:47:45 +0700

    Update folder structure

---
+++ src/Nova/Folder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Soap\VimeoVideoManager\Nova;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Resource;
+
+
+class FolderResource extends Resourc
+{
+
+}
---
+++ src/Nova/Video.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Soap\VimeoVideoManager\Nova;
+
+use Laravel\Nova\Nova;
+use Laravel\Nova\Resource;
+
+class VideoResource extends Resource
+{
+    
+}
